49199712c363cc6f99524da770c4bf8c0fc17a3a
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-21 15:55:00 -0800

    book-list component created and renderList() function has implemented using map which shows title of each book

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import BOoksReducer from './reducer_books';
+import BooksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
   books: BooksReducer
