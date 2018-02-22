c80851051482ce5b55a68607778863c10423a2f3
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-21 17:29:15 -0800

    our onClick method attribute added as selectBook action

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -13,7 +13,12 @@ class BookList extends Component {
 		return this.props.books.map((book) => {
 
 			return (
-				<li key={book.title} className="list-group-item">{book.title}</li>
+				<li 
+					key={book.title} 
+					onClick={() => this.props.selectBook(book)}
+					className="list-group-item">
+					{book.title}
+				</li>
 			);
 		});
 	}
@@ -40,5 +45,19 @@ function mapStateToProps(state) {
 }
 
 
-export default connect(mapStateToProps)(BookList);
+//Anything returned from this function will end up as props
+//on the BookList container
+function mapDispatchToProps(dispatch) {
+	//Whenever selectBook is called, the result should be passed to 
+	//all of our reducers
+	//selectBook is a action creator we imported
+	//result flows through dispatch function, and dispatch function 
+	//takes all the actions and spits back to all the reducers
+	return bindActionCreators({ selectBook: selectBook }. dispatch)
+}
+
+
+// Promote BookList from a compnent to a container - it needs to know
+//about this new dispatch method, selectBook. Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
 //connect takes a function, and component to produce a container
