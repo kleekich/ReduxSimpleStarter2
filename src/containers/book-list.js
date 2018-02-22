1c9b8f2550fd3c6ce1ccee24443980aa6c6ec928
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-21 16:20:11 -0800

    added some notes

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { connect} from 'react-redux';
+import { connect } from 'react-redux'; 
+//glue between React and Redux, and makes connect function available.
 
 //This is a container that has direct access from Redux.
 class BookList extends Component {
@@ -21,3 +22,20 @@ class BookList extends Component {
 		)
 	}
 }
+/*
+Whenever state ever changes, 
+1. this container automatically re-render with new state.books
+2. the object in the state function will be asgined to props
+*/
+function mapStateToProps(state) {
+	//Whatever is returned will show up as props inside of BookLists
+
+	return {
+		books: state.books
+		//because reducer returns books we set it to state.books
+	};
+}
+
+
+export default connect(mapSTateToProps)(BookList);
+//connect takes a function, and component to produce a container
