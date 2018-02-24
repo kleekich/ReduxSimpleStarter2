1286fe1f2a598abfd54aa908f0d6da36ed8588e4
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-24 11:50:12 -0800

    Our books data gets value for pages and it is shown in our book-detail container

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -11,10 +11,10 @@
 */
 export default function() {
 	return [
-		{title: 'Javascript: The Good parts'},
-		{title: 'Harry Potter'},
-		{title: 'THe Dark Tower'},
-		{title: 'Eloquent Ruby'}
+		{title: 'Javascript: The Good parts', pages: 101},
+		{title: 'Harry Potter', pages: 91},
+		{title: 'THe Dark Tower', pages: 11},
+		{title: 'Eloquent Ruby', pages: 1}
 	]
 
 }
